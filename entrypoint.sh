4e31b2e8dc7d6894f793bd1eda1f7b93b0090cec
Tomo Suzuki <suztomo@google.com>
2020-12-14 11:57:47 -0500

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ echo "::set-output name=time::$time"
 
 JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
+export
 
 echo "Building it with Gradle"
 ./gradlew build publishToMavenLocal -x test -x signMavenJavaPublication
@@ -13,8 +14,10 @@ echo "Building it with Gradle"
 echo "Content of home:"
 ls -al ~/
 
+sleep 2
+
 echo "Finding pom files"
-find ~/.m2/repository -name "gax*.pom"
+find $HOME/.m2 -name "gax*.pom"
 
 java -jar /cloud-opensource-java/linkage-monitor/target/linkage-monitor-*-all-deps.jar
 
