df4e480c66dc704cf6f4114df8e1256097c43773
Tomo Suzuki <suztomo@google.com>
2020-12-29 14:26:47 -0500

    Not specifying Java version

--- linkage-monitor/linkage-monitor.sh
+++ linkage-monitor/linkage-monitor.sh
@@ -1,38 +1,8 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
-
-JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export PATH=$JAVA_HOME/bin:$PATH
-# For Gradle plugin to install ~/.m2/repository
-printenv
-
-# $HOME (/github/home/.m2, = /home/runner/work/_temp/_github_home/.m2) is a symbolic link
-# ln -s $HOME/.m2 /root/.m2
-
-# mvn -v
-
-# echo "Building it with Gradle"
-# This installs user.home system property which is somehow /root in Docker
-# ./gradlew build publishToMavenLocal -x test -x signMavenJavaPublication
-
-echo "Content of ~ (empty)"
-ls -al ~/
-
-echo "Content of HOME (empty)"
-ls -al $HOME
-
-sleep 2
-
-# echo "Finding pom files under /root/.m2/repository"
-# find /root/.m2/repository -name "gax*.pom"
-
-# $HOME is mounted by docker command and .m2 in it is a symbolic link to /home/runner/.m2 (outside
-# the container)
-echo "Searching for pom files (gax*.pom) under $HOME/.m2/repository"
-find $HOME/.m2/repository -name "gax*.pom"
+echo "Using JAVA_HOME: $JAVA_HOME"
+which java
+java -version
 
 echo "Changing directory to $GITHUB_WORKSPACE"
 cd $GITHUB_WORKSPACE
