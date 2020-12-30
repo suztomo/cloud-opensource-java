3aefadb17adf48072cb9b2a35e8e16d6c5ff47cb
Tomo Suzuki <suztomo@google.com>
2020-12-30 11:12:05 -0500

    Removed unnecessary output

--- linkage-monitor/linkage-monitor.sh
+++ linkage-monitor/linkage-monitor.sh
@@ -8,13 +8,4 @@ java -version
 echo "Changing directory to $GITHUB_WORKSPACE"
 cd $GITHUB_WORKSPACE
 
-echo "Listing environment variable"
-printenv
-
-echo "Searching for pom files (pom.xml) under current working directory"
-find . -name 'pom.xml'
-
-echo "Content of current working directory"
-ls -al
-
 java -jar ${GITHUB_ACTION_PATH}/target/linkage-monitor-*-all-deps.jar com.google.cloud:libraries-bom
